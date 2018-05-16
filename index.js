175a7f8c8514c1850314da07246124ca0fc1907c
yyli <yyli>
2018-05-16 18:11:27 +0800

    JWT server side ok

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
-require('./routes/authRoutes')(app);
+const auth = require('./services/passport')();
+const users = require('./models/users');
+const jwt = require("jwt-simple");
+const keys = require('./config/keys.js');
+
+app.use(bodyParser.json());
+app.use(auth.initialize());
 
 app.get('/', (req,res)=>{
-  res.send('Hi there');
+  res.send({
+    status: 'Service is alive!'
+  });
+});
+
+app.get("/user", auth.authenticate(), function(req, res) {
+    console.log("get /user:", req.user);
+    var user = users.find(function(u) {
+        return u.id === req.user.id;
+    });
+
+    res.json(user);
+});
+
+app.post("/token", function(req, res) {
+    console.log("post /token: ", req.body);
+    if (req.body.email && req.body.password) {
+        var email = req.body.email;
+        var password = req.body.password;
+        var user = users.find(function(u) {
+            return u.email === email && u.password === password;
+        });
+        if (user) {
+            var payload = {
+                id: user.id
+            };
+            var token = jwt.encode(payload, keys.jwtSecret);
+            res.json({
+                token: token
+            });
+        } else {
+            res.sendStatus(401);
+        }
+    } else {
+        res.sendStatus(401);
+    }
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT,()=>console.log("Listening on port", PORT));
--- services/passport.js
+++ services/passport.js
@@ -1,28 +1,14 @@
 // auth.js
 var passport = require('passport');
 var passportJWT = require('passport-jwt');
-var cfg = require('././config/keys.js');
+var keys = require('../config/keys.js');
 var ExtractJwt = passportJWT.ExtractJwt;
 var Strategy = passportJWT.Strategy;
+var users = require('../models/users');
 var params = {
   secretOrKey: keys.jwtSecret,
-  jwtFromRequest: ExtractJwt.fromAuthHeader()
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt")
 };
-
-var users = [
-  {
-    id: 1,
-    name: 'John',
-    email: 'john@mail.com',
-    password: 'john123'
-  },
-  {
-    id: 2,
-    name: 'Sarah',
-    email: 'sarah@mail.com',
-    password: 'sarah123'
-  }
-];
 
 module.exports = function() {
   var strategy = new Strategy(params, function(payload, done) {
@@ -41,7 +27,7 @@ module.exports = function() {
       return passport.initialize();
     },
     authenticate: function() {
-      return passport.authenticate('jwt', cfg.jwtSession);
+      return passport.authenticate('jwt', keys.jwtSession);
     }
   };
 };
